ee9c644472886cac5ad88bc777f505741b3ab33b
Ilya Sevostyanov <d3th@zeen.ru>
2016-04-15 23:52:28 +0300

    Update main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,227 @@
-var game = new Phaser.Game(1152, 768, Phaser.AUTO, '', {preload: preload, create: create, update: update});
-var blocks;
-var triangles;
-var player = null;
-var finish = null;
-var gameLevel = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [1,0,0,3,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-];
-var cursors;
-var playerSpeed = 600;
+var game = new Phaser.Game(1152, 768, Phaser.CANVAS, 'game');
 
-function preload() {
-    game.load.image('grid', 'assets/img/grid.png');
-    game.load.image('block', 'assets/img/block.png');
-    game.load.image('player', 'assets/img/player.png');
-    game.load.image('triangle', 'assets/img/triangle.png');
-}
+var PhaserGame = function (game) {
 
-function create() {
-    game.add.sprite(0, 0, 'grid');
-    blocks = game.add.group();
-    blocks.enableBody = true;
-    triangles = game.add.group();
-    triangles.enableBody = true;
-    for (var i = 0; i < 12; i++) {
-        for (var j = 0; j < 18; j++) {
-            switch (gameLevel[i][j]) {
-                case 1:
-                    blocks.create(j*64, i*64, 'block').body.immovable = true;
-                    break;
-                case 2:
-                    player = game.add.sprite(j * 64, i * 64, 'player');
-                    game.physics.arcade.enable(player);
-                    player.body.collideWorldBounds = true;
-                    break;
-                case 3:
-                    finish = game.add.group();
-                    finish.enableBody = true;
-                    finish.create(j*64, i*64, 'player');
-                    break;
-                case 4: // triangle top-right
-                case 5: // triangle right-bottom
-                case 6: // triangle bottom-left
-                case 7: // triangle left-top
-                    var rotation = (gameLevel[i][j] - 4) * 90;
-                    var x = j*64;
-                    var y = i*64;
-                    triangles.create(x, y, 'triangle').angle = rotation;
-                    break;
+    this.map = null;
+    this.layer = null;
+    this.player = null;
+
+    this.safetile = 1;
+    this.gridsize = 64;
+
+    this.speed = 150;
+    this.threshold = 3;
+    this.turnSpeed = 150;
+
+    this.marker = new Phaser.Point();
+    this.turnPoint = new Phaser.Point();
+
+    this.directions = [null, null, null, null, null];
+    this.opposites = [Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP];
+
+    this.current = Phaser.UP;
+    this.turning = Phaser.NONE;
+
+    this.playerSpeed = 600;
+    this.player = null;
+    this.finish = null;
+
+    this.gameLevel = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+
+    this.finishHandler = function (player, finish) {
+        if (Math.round(player.position.x / 10) == Math.round(finish.position.x / 10) && Math.round(player.position.y / 10) == Math.round(finish.position.y / 10)) {
+            finish.kill();
+        }
+    }
+};
+
+PhaserGame.prototype = {
+
+    init: function () {
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+    },
+
+    preload: function () {
+        this.load.image('grid', 'assets/img/grid.png');
+        this.load.image('block', 'assets/img/block.png');
+        this.load.image('player', 'assets/img/player.png');
+        this.load.image('triangle', 'assets/img/triangle.png');
+    },
+
+    create: function () {
+        this.add.sprite(0, 0, 'grid');
+        this.blocks = this.add.group();
+        this.blocks.enableBody = true;
+        this.triangles = this.add.group();
+        this.triangles.enableBody = true;
+        for (var i = 0; i < 12; i++) {
+            for (var j = 0; j < 18; j++) {
+                switch (this.gameLevel[i][j]) {
+                    case 1:
+                        this.blocks.create(j * 64, i * 64, 'block').body.immovable = true;
+                        break;
+                    case 2:
+                        this.player = this.add.sprite(j * 64, i * 64, 'player');
+                        this.physics.arcade.enable(this.player);
+                        this.player.body.collideWorldBounds = true;
+                        break;
+                    case 3:
+                        this.finish = this.add.group();
+                        this.finish.enableBody = true;
+                        this.finish.create(j * 64, i * 64, 'player');
+                        break;
+                    case 4: // triangle top-right
+                    case 5: // triangle right-bottom
+                    case 6: // triangle bottom-left
+                    case 7: // triangle left-top
+                        var rotation = (this.gameLevel[i][j] - 4) * 90;
+                        var x = j * 64;
+                        var y = i * 64;
+                        this.triangles.create(x, y, 'triangle').angle = rotation;
+                        break;
+                }
             }
         }
-    }
 
-    cursors = game.input.keyboard.createCursorKeys();
-}
+        cursors = this.input.keyboard.createCursorKeys();
+    },
 
-function update() {
-    if (!player.body.velocity.y && !player.body.velocity.x) {
-        if (cursors.down.isDown) {
-            player.body.velocity.y = playerSpeed;
-        }
-        else if (cursors.up.isDown) {
-            player.body.velocity.y = -playerSpeed;
-        }
-        else if (cursors.left.isDown) {
-            player.body.velocity.x = -playerSpeed;
-        }
-        else if (cursors.right.isDown) {
-            player.body.velocity.x = playerSpeed;
-        }
-    }
-    game.physics.arcade.collide(player, blocks);
-    //game.physics.arcade.collide(player, finish);
-    game.physics.arcade.overlap(player, finish, finishHandler, null, this);
-    game.physics.arcade.overlap(player, triangles, finishHandler, null, this);
-}
+    checkKeys: function () {
 
-function finishHandler(player, finish) {
-    if (Math.round(player.position.x/10) == Math.round(finish.position.x/10) && Math.round(player.position.y/10) == Math.round(finish.position.y/10)) {
-        finish.kill();
+        if (this.cursors.left.isDown && this.current !== Phaser.LEFT) {
+            this.checkDirection(Phaser.LEFT);
+        }
+        else if (this.cursors.right.isDown && this.current !== Phaser.RIGHT) {
+            this.checkDirection(Phaser.RIGHT);
+        }
+        else if (this.cursors.up.isDown && this.current !== Phaser.UP) {
+            this.checkDirection(Phaser.UP);
+        }
+        else if (this.cursors.down.isDown && this.current !== Phaser.DOWN) {
+            this.checkDirection(Phaser.DOWN);
+        }
+        else {
+            //  This forces them to hold the key down to turn the corner
+            this.turning = Phaser.NONE;
+        }
+
+    },
+
+    checkDirection: function (turnTo) {
+
+        if (this.turning === turnTo || this.directions[turnTo] === null || this.directions[turnTo].index !== this.safetile) {
+            //  Invalid direction if they're already set to turn that way
+            //  Or there is no tile there, or the tile isn't index a floor tile
+            return;
+        }
+
+        //  Check if they want to turn around and can
+        if (this.current === this.opposites[turnTo]) {
+            this.move(turnTo);
+        } else {
+            this.turning = turnTo;
+
+            this.turnPoint.x = (this.marker.x * this.gridsize) + (this.gridsize / 2);
+            this.turnPoint.y = (this.marker.y * this.gridsize) + (this.gridsize / 2);
+        }
+
+    },
+
+    turn: function () {
+
+        var cx = Math.floor(this.player.x);
+        var cy = Math.floor(this.player.y);
+
+        //  This needs a threshold, because at high speeds you can't turn because the coordinates skip past
+        if (!this.math.fuzzyEqual(cx, this.turnPoint.x, this.threshold) || !this.math.fuzzyEqual(cy, this.turnPoint.y, this.threshold)) {
+            return false;
+        }
+
+        this.player.x = this.turnPoint.x;
+        this.player.y = this.turnPoint.y;
+
+        this.player.body.reset(this.turnPoint.x, this.turnPoint.y);
+
+        this.move(this.turning);
+
+        this.turning = Phaser.NONE;
+
+        return true;
+
+    },
+
+    move: function (direction) {
+
+        var speed = this.speed;
+
+        if (direction === Phaser.LEFT || direction === Phaser.UP) {
+            speed = -speed;
+        }
+
+        if (direction === Phaser.LEFT || direction === Phaser.RIGHT) {
+            this.player.body.velocity.x = speed;
+        } else {
+            this.player.body.velocity.y = speed;
+        }
+
+        this.add.tween(this.player).to({angle: this.getAngle(direction)}, this.turnSpeed, "Linear", true);
+
+        this.current = direction;
+
+    },
+
+    getAngle: function (to) {
+
+        //  About-face?
+        if (this.current === this.opposites[to]) {
+            return "180";
+        }
+
+        if ((this.current === Phaser.UP && to === Phaser.LEFT) ||
+            (this.current === Phaser.DOWN && to === Phaser.RIGHT) ||
+            (this.current === Phaser.LEFT && to === Phaser.DOWN) ||
+            (this.current === Phaser.RIGHT && to === Phaser.UP)) {
+            return "-90";
+        }
+
+        return "90";
+
+    },
+
+    update: function () {
+
+        if (!this.player.body.velocity.y && !this.player.body.velocity.x) {
+            if (cursors.down.isDown) {
+                this.player.body.velocity.y = this.playerSpeed;
+            }
+            else if (cursors.up.isDown) {
+                this.player.body.velocity.y = -this.playerSpeed;
+            }
+            else if (cursors.left.isDown) {
+                this.player.body.velocity.x = -this.playerSpeed;
+            }
+            else if (cursors.right.isDown) {
+                this.player.body.velocity.x = this.playerSpeed;
+            }
+        }
+
+        game.physics.arcade.collide(this.player, this.blocks);
+        game.physics.arcade.overlap(this.player, this.finish, this.finishHandler, null, this);
     }
-}
+};
+
+game.state.add('Game', PhaserGame, true);
